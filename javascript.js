cecea19b08bfedd37dcd8ad998e283a2808264f1
Cresk <creskopper@gmail.com>
2022-08-18 12:08:49 +0100

    Add playRound function to play a single round

--- javascript.js
+++ javascript.js
@@ -6,3 +6,58 @@ function getComputerChoice() {
     return choices[randomIndex];
 }
 
+function playRound(playerSelection, computerSelection) {
+
+    let choice = computerSelection;
+    let result;
+
+    if(playerSelection = "Rock") {
+
+        switch(choice) {
+            case "Paper" :
+                result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                break;
+
+            case "Scissors" :
+                result = `You Win! ${playerSelection} beats ${computerSelection}`;
+                break;
+
+            default : result = "It's a tie!"
+        }
+    }
+
+    else if(playerSelection = "Paper") {
+
+        switch(choice) {
+            case "Scissors" :
+                result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                break;
+
+            case "Rock" :
+                result = `You Win! ${playerSelection} beats ${computerSelection}`;
+                break;
+
+            default : result = "It's a tie!"
+        }
+        
+    }
+
+    else if(playerSelection = "Scissors") {
+
+        switch(choice) {
+            case "Rock" :
+                result = `You Lose! ${computerSelection} beats ${playerSelection}`;
+                break;
+
+            case "Paper" :
+                result = `You Win! ${playerSelection} beats ${computerSelection}`;
+                break;
+
+            default : result = "It's a tie!"
+        }
+        
+    }
+
+    return result;
+
+}
