8fdf2426367c2e6a359990b8449b5f082f158bcd
Cresk <creskopper@gmail.com>
2022-08-30 18:22:38 +0100

    Rewrite game functions
    Rework and rewrite all functions for better readability

--- javascript.js
+++ javascript.js
@@ -1,61 +1,3 @@
-function getComputerChoice() {
-
-    let choices = ["Rock", "Paper", "Scissors"];
-    let randomIndex = Math.floor(Math.random() * 3);
-
-    return choices[randomIndex];
-}
-
-// Play one round
-function playRound(playerSelection, computerSelection) {
-    let result = "";
-
-    if(playerSelection == "Rock") {
-
-        switch(computerSelection) {
-            case "Paper" :
-                result = `You Lose! ${computerSelection} beats ${playerSelection}`;
-                break;
-
-            case "Scissors" :
-                result = `You Win! ${playerSelection} beats ${computerSelection}`;
-                break;
-
-            default : result = `It's a tie! ${playerSelection} vs ${computerSelection}`;
-        }
-    } else if(playerSelection == "Paper") {
-
-        switch(computerSelection) {
-            case "Scissors" :
-                result = `You Lose! ${computerSelection} beats ${playerSelection}`;
-                break;
-
-            case "Rock" :
-                result = `You Win! ${playerSelection} beats ${computerSelection}`;
-                break;
-
-            default : result = `It's a tie! ${playerSelection} vs ${computerSelection}`;
-        }
-        
-    } else if(playerSelection == "Scissors") {
-
-        switch(computerSelection) {
-            case "Rock" :
-                result = `You Lose! ${computerSelection} beats ${playerSelection}`;
-                break;
-
-            case "Paper" :
-                result = `You Win! ${playerSelection} beats ${computerSelection}`;
-                break;
-
-            default : result = `It's a tie! ${playerSelection} vs ${computerSelection}`;
-        }
-        
-    }
-
-    return result;
-}
-
 // DOM Variables
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
@@ -70,121 +12,98 @@ const computerFinalScore = document.getElementById("final-computer-score");
 const gameWrapper = document.getElementById("game-wrapper");
 const playAgainBtn = document.getElementById("play-again-btn");
 
-
 let playerScore = 0;
 let computerScore = 0;
 
-// Play until one player reaches 5 points
-function roundButton(choice) {
-    computerSelection = getComputerChoice();
-    let oneRoundResult = playRound(choice, computerSelection);
-    
-    paraResults.textContent = `${oneRoundResult}`;
-
-    if(oneRoundResult.includes("You Win")) {
-        playerScore += 1;
-        paraPlayerScore.textContent = `Player Score: ${playerScore} Points`;
-    } else if(oneRoundResult.includes("You Lose")) {
-        computerScore += 1;
-        paraComputerScore.textContent = `Computer Score: ${computerScore} Points`;
-    } else if(oneRoundResult.includes("It's a tie")) {
-        playerScore += 0;
-        computerScore += 0;
-    }
-
-    function gameResult() {
-        if(playerScore == 5) {
-            
-            headingResult.textContent = "Congratulations! You Win!";
-            playerFinalScore.textContent = `Player Score: ${playerScore} Points`;
-            computerFinalScore.textContent = `Player Score: ${computerScore} Points`;
-            gameWrapper.style.visibility = "hidden";
-            resultWrapper.style.visibility = "visible";
-
-            playerScore = 0;
-            computerScore = 0;
-
-            paraPlayerScore.textContent = `Player Score: ${playerScore} Points`;
-            paraComputerScore.textContent = `Computer Score: ${computerScore} Points`;
-
-            return console.log("Congratulations! You Win!");
-        } else if(computerScore == 5) {
-
-            headingResult.textContent = "Computer Won!";
-            playerFinalScore.textContent = `Player Score: ${playerScore} Points`;
-            computerFinalScore.textContent = `Computer Score: ${computerScore} Points`;
-            gameWrapper.style.visibility = "hidden";
-            resultWrapper.style.visibility = "visible";
-
-            playerScore = 0;
-            computerScore = 0;
-
-            paraPlayerScore.textContent = `Player Score: ${playerScore} Points`;
-            paraComputerScore.textContent = `Computer Score: ${computerScore} Points`;
-
-            return console.log("Computer Won!");
-        } else return;
-    }
-    gameResult();
-}
-
-
-
 rockBtn.addEventListener("click", () => {
-    roundButton("Rock");
+    oneRoundResult = playRound("Rock", getComputerChoice());
+    oneRoundResultText = roundResultText(oneRoundResult);
+    gameText();
+    winnerAnnounce();
 });
-
 paperBtn.addEventListener("click", () => {
-    roundButton("Paper");
+    oneRoundResult = playRound("Paper", getComputerChoice());
+    oneRoundResultText = roundResultText(oneRoundResult);
+    gameText();
+    winnerAnnounce();
 });
-
 scissorsBtn.addEventListener("click", () => {
-    roundButton("Scissors");
+    oneRoundResult = playRound("Scissors", getComputerChoice());
+    oneRoundResultText = roundResultText(oneRoundResult);
+    gameText();
+    winnerAnnounce();
 });
-
 playAgainBtn.addEventListener("click", () => {
     gameWrapper.style.visibility = "visible";
     resultWrapper.style.visibility = "hidden";
     paraResults.textContent = "Choose your weapon!"
     playerScore = 0;
     computerScore = 0;
-})
+});
 
 
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
 
+function playRound(playerSelection, computerSelection) {
+    if(
+        (playerSelection == "Rock" && computerSelection == "Scissors") ||
+        (playerSelection == "Paper" && computerSelection == "Rock") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper")) { return 1 }
+    
+    else if(
+        (playerSelection == "Rock" && computerSelection == "Paper") ||
+        (playerSelection == "Paper" && computerSelection == "Scissors") ||
+        (playerSelection == "Scissors" && computerSelection == "Rock")) { return 0 }
+    
+    else if(playerSelection == computerSelection) { return -1 }
+}
 
+function roundResultText(num) {
+    if(num == 1) {
+        playerScore += 1;
+        return "You win this round!"
+    } else if(num == 0) {
+        computerScore += 1;
+        return "You lost this round!"
+    } else if(num == -1) {
+        playerScore += 0;
+        computerScore += 0;
+        return "No winner for this round!"
+    }
+}
 
+function gameText() {
+    paraPlayerScore.textContent = `Player Score: ${playerScore} Points`;
+    paraComputerScore.textContent = `Computer Score: ${computerScore} Points`;
+    paraResults.textContent = oneRoundResultText;
+}
 
-//  // Display the game result
-
-
-// game()
-// function game() {
-
-
-//     let playerScore = 0;
-//     let computerScore = 0;
-
-//     computerSelection = getComputerChoice();
-
-//     let oneRoundResult = playRound(playerSelection, computerSelection);
-//     console.log(oneRoundResult);
-
-//     if(oneRoundResult.includes("You Win")) {
-//         playerScore += 1;
-//     }
-//     else if(oneRoundResult.includes("You Lose")) {
-//         computerScore += 1;
-//     }
-//     else if(oneRoundResult.includes("It's a tie")) {
-//         playerScore += 0;
-//         computerScore += 0;
-//     }
-
-
-   
-
-//     console.log(gameResult());
-
-// }
+function winnerAnnounce() {
+    if(playerScore == 5) {
+        headingResult.textContent = "Congratulations! You Win!";
+        playerFinalScore.textContent = `Player Score: ${playerScore} Points`;
+        computerFinalScore.textContent = `Computer Score: ${computerScore} Points`;
+        gameWrapper.style.visibility = "hidden";
+        resultWrapper.style.visibility = "visible";
+        playerScore = 0;
+        computerScore = 0;
+        paraPlayerScore.textContent = `Player Score: ${playerScore} Points`;
+        paraComputerScore.textContent = `Computer Score: ${computerScore} Points`;
+    } else if(computerScore == 5) {
+        headingResult.textContent = "Computer Won!";
+        playerFinalScore.textContent = `Player Score: ${playerScore} Points`;
+        computerFinalScore.textContent = `Computer Score: ${computerScore} Points`;
+        gameWrapper.style.visibility = "hidden";
+        resultWrapper.style.visibility = "visible";
+        playerScore = 0;
+        computerScore = 0;
+        paraPlayerScore.textContent = `Player Score: ${playerScore} Points`;
+        paraComputerScore.textContent = `Computer Score: ${computerScore} Points`;
+    }
+    
+}
 
